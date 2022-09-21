47c270894d43733742991cde2fa3f16a6431d010
Alex-Zongo <alexanicetzongo@gmail.com>
2022-09-22 00:42:45 +0800

    attempt to define pid controller

--- uthai_gazebo/launch/gazebo.launch.py
+++ uthai_gazebo/launch/gazebo.launch.py
@@ -14,10 +14,18 @@ import xacro
 
 
 def generate_launch_description():
+    world = os.path.join(get_package_share_directory(
+        'uthai_gazebo'), 'worlds', 'empty.world')
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py'])
+            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+        launch_arguments={'world': world}.items()
     )
+
+    # gazebo_custom_world = ExecuteProcess(
+    #     cmd=['gazebo', '--verbose', '-s',
+    #          'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', '-u']
+    # )
 
     package_path = os.path.join(
         get_package_share_directory('uthai_description')
@@ -43,7 +51,7 @@ def generate_launch_description():
                    '-entity', 'uthai',
                    '-x', '0',
                    '-y', '0',
-                   '-z', '0',
+                   '-z', '0.745',
                    ],
         output='screen'
     )
@@ -60,6 +68,18 @@ def generate_launch_description():
         output='screen'
     )
 
+    load_left_leg_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'uthai_l_leg_controller'],
+        output='screen'
+    )
+
+    load_right_leg_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'uthai_r_leg_controller'],
+        output='screen'
+    )
+
     return LaunchDescription([
         RegisterEventHandler(
             event_handler=OnProcessExit(
@@ -67,10 +87,22 @@ def generate_launch_description():
                 on_exit=[load_joint_state_controller],
             )
         ),
+        # RegisterEventHandler(
+        #     event_handler=OnProcessExit(
+        #         target_action=load_joint_state_controller,
+        #         on_exit=[load_uthai_controller],
+        #     )
+        # ),
         RegisterEventHandler(
             event_handler=OnProcessExit(
                 target_action=load_joint_state_controller,
-                on_exit=[load_uthai_controller],
+                on_exit=[load_left_leg_controller],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_left_leg_controller,
+                on_exit=[load_right_leg_controller],
             )
         ),
         gazebo,
